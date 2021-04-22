24e8dbcf1aa73ad95c250f7aebcd1046e3b52c0f
XiaoMing <xiaominglovejava@163.com>
2021-04-22 14:36:28 +0800

    Shopping第一次提交

---
+++ account/src/main/java/eamon/springcloud/account/AccountApplication.java
@@ -0,0 +1,13 @@
+package eamon.springcloud.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
---
+++ account/src/test/java/eamon/springcloud/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package eamon.springcloud.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
