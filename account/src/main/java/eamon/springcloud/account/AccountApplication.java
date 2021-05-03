deb206822843d9f5b87404eb4a9380656dc91496
EamonYin <xiaominglovejava@163.com>
2021-05-03 16:22:15 +0800

    修复nacos无法发现nacos-provide模块，以及Maven依赖存在问题

--- account/src/main/java/eamon/springcloud/account/AccountApplication.java
+++ account/src/main/java/eamon/springcloud/account/AccountApplication.java
@@ -1,9 +1,14 @@
 package eamon.springcloud.account;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +17,20 @@ public class AccountApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/consumer")
+    public String test1() {
+        return restTemplate.getForObject("http://nacos_provide/helloNacos", String.class);
     }
 
 }
